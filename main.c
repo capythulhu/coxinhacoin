5f6b02f04f680a8221c7e0adfad20959bc666c1e
Thiago <thiagozoid@gmail.com>
2020-03-06 18:58:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,18 @@
 #define HASH_LENGTH 8
 
 unsigned char *hash(unsigned char *input){
+    const static unsigned char randomBytes[] = { 0x7c, 0x16, 0xc3, 0xb7, 0x00, 0xf9, 0xec, 0xaa };
+    const static unsigned short int randomBytesLength = sizeof(randomBytes);
+
     unsigned char *digest = malloc(sizeof(char) * HASH_LENGTH);
     memset(digest, 0, HASH_LENGTH);
     
     int size = strlen(input), result = 0;
-    int i;
+    int i, j;
     for(i = 0; i < size; i++){
-        digest[i % HASH_LENGTH] = input[i] + i;
+        for(j = 0; j < HASH_LENGTH; j++){
+            digest[i % HASH_LENGTH] = input[i] + i ^ randomBytes[(input[i] + i) % randomBytesLength];  
+        }
         result ^= input[i] + i;
     }
     for(i = 0; i < HASH_LENGTH; i++){
